4fca83fe7e3adf8f2399ca8e4520d4046dea29b7
unknown <karimowrinat07@gmail.com>
2026-09-18 15:21:07 +0500

    Выполнил задание 2

--- src/task2-predicate.ts
+++ src/task2-predicate.ts
@@ -1,3 +1,4 @@
+import { moveAnimal } from './task1-narrowing';
 // Тип банковской транзакции (пока без interface, используем type)
 export type Transaction = {
   id: string;
@@ -11,12 +12,33 @@ export type Transaction = {
 // 2. Что у data есть поля id, amount, type
 // 3. Что id - это строка, amount - число, type - одна из двух строк
 export function isTransaction(data: unknown): data is Transaction {
-  // Напишите код здесь
+  if (typeof data !== 'object') {
+    return false;
+  }
+  if (data === null) {
+    return false;
+  }
+  const obj = data as Record<string, unknown>;
+  if (typeof obj.id !== 'string') {
+    return false;
+  }
+  if (typeof obj.amount !== 'number') {
+    return false;
+  }
+  if (obj.type !== 'deposit' && obj.type !== 'withdrawal') {
+    return false;
+  }
+  return true;
 }
 
 // Функция обработки. 
 // Если data это транзакция - вернуть "Обработана транзакция на сумму <amount>"
 // Иначе вернуть "Неизвестные данные"
 export function processTransaction(data: unknown): string {
-  // Напишите код здесь, используя isTransaction
+  if (isTransaction(data)) {
+    return "Обработана транзакция на сумму " + data.amount;
+  }
+  else {
+    return "Неизвестные данные";
+  }
 }
