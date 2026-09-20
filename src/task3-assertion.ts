7fb2155b346bc1b5d50cf78f8d8900a96414c8c8
unknown <karimowrinat07@gmail.com>
2026-09-21 00:04:25 +0500

    Выполнил задание 3

--- src/task3-assertion.ts
+++ src/task3-assertion.ts
@@ -8,12 +8,25 @@ export type AppConfig = {
 // Если данные не соответствуют AppConfig - бросить Error("Invalid config")
 // Используйте синтаксис: asserts data is AppConfig
 export function assertValidConfig(data: unknown): asserts data is AppConfig {
-  // Напишите код здесь
+  if (typeof data !== "object") {
+    throw new Error ("Invalid config");
+  }
+  if (data === null) {
+    throw new Error ("Invalid config");
+  }
+  const obj = data as Record<string, unknown>;
+  if (typeof obj.apiUrl !== "string") {
+    throw new Error ("Invalid config");
+  }
+  if (typeof obj.timeout !== "number") {
+    throw new Error ("Invalid config");
+  }
 }
 
 // Функция инициализации.
 // Сначала вызывает assertValidConfig(config).
 // Если проверка прошла - вернуть "API: <apiUrl>, Timeout: <timeout>"
 export function initApp(config: unknown): string {
-  // Напишите код здесь
+  assertValidConfig(config);
+  return `API: ${config.apiUrl}, Timeout: ${config.timeout}`;
 }
